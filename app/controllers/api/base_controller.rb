2bc7cf593f4ef7d3a1076011fc13f58d5bc7159b
Giselelasserre <giseleslasserre@gmail.com>
2017-09-20 11:52:04 -0300

    controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,14 +1,6 @@
-require 'json'
-require 'open-uri'
 
 class Api::V1::BaseController < ActionController::Base
 
-  puts "Escolha uma linguagem de programação ?"
-  language = gets.chomp
-  url = " https://api.github.com/search/code?q=addClass+in:file+language:#{language}+repo:#{language}/#{language} "
-  raw_json = open(url).read
-  user = JSON.parse(raw_json)
-  puts "A #{language} tem #{user["public_repos"]} repositórios"
 
 end
 
