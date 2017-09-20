8bfdd70efc02f9740376a7914ac0f31111aa1594
Giselelasserre <giseleslasserre@gmail.com>
2017-09-20 08:53:49 -0300

    load list page

--- app/controllers/api/repositories_controller.rb
+++ app/controllers/api/repositories_controller.rb
@@ -1,11 +1,23 @@
 
 class Api::V1::RepositoriesController < Api::V1::BaseController
   def index
-  @repositories = Repository.all
+  github = Github.new
+  github.repos.list user: 'user-name'
+  github.repos.list user: 'user-name', { |repo| ... }
   end
 
   def show
-  @repository = Repository.find(params[:id])
+
+    if html_url.present? ("Jquery","CSS", "HTML", "Java")
+      github.repos.get_by_html_url 'html_url'
+      redirect_to _load_list_path
+
+     else
+      return "No one Repository with this language "
+    end
+
   end
 
 end
+
+
