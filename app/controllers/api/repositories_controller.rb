0c9628749a9c975b7ac5d2cc8a5b6996abd2c9a4
Giselelasserre <giseleslasserre@gmail.com>
2017-09-20 11:32:43 -0300

    ajud

--- app/controllers/api/repositories_controller.rb
+++ app/controllers/api/repositories_controller.rb
@@ -1,23 +1,20 @@
 
 class Api::V1::RepositoriesController < Api::V1::BaseController
   def index
-  github = Github.new
-  github.repos.list user: 'user-name'
-  github.repos.list user: 'user-name', { |repo| ... }
+    @repositories = Repository.all
   end
 
   def show
 
-    if html_url.present? ("Jquery","CSS", "HTML", "Java")
-      github.repos.get_by_html_url 'html_url'
-      redirect_to _load_list_path
+    @repository = Repository.find(params[:language])
+  end
 
-     else
-      return "No one Repository with this language "
-    end
-
+  def create
+  # a variavel "language" tem que vir a partir do submit...
+  url = "https://api.github.com/search/repositories?q=language:#{language}"
+  response = HTTParty.get(url)
+  repositories = response.parsed_response["items"]
   end
 
 end
-
 
